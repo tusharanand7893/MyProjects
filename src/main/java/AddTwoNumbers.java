1d8e32a775f3e39af605e3796bc7ecc03a4f4748
tusharanand7893 <tushar.anand7893@gmail.com>
2023-03-13 14:27:32 +0530

    Added programs for adding element of list

---
+++ src/main/java/AddTwoNumbers.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddTwoNumbers {
+
+    public static void main(String[] args) {
+        List<Integer> l1 = new ArrayList<>();
+        l1.add(1);
+        l1.add(2);
+        l1.add(4);
+        l1.add(6);
+
+        List l2  = new ArrayList<>();
+        l2.add(1);
+        l2.add(2);
+        l2.add(4);
+        l2.add(5);
+
+        System.out.println(sumOfTwoNumbers(l1,l2));
+    }
+
+    public static List<Integer> sumOfTwoNumbers(List<Integer> l1,List<Integer> l2){
+        List<Integer> l=new ArrayList<>();
+        int count=0;
+        if(l1.size()> l2.size()){
+        while(count<l2.size()){
+            l.add(l1.get(count)+ l2.get(count));
+            count++;
+        }
+        for(int i=l2.size();i< l1.size();i++){
+            l.add(l1.get(i));
+        }
+
+        }
+        else{
+            while(count<l1.size()){
+                l.add(l1.get(count)+ l2.get(count));
+                count++;
+            }
+            for(int i=l1.size();i< l2.size();i++){
+                l.add(l2.get(i));
+            }
+
+        }
+        return l;
+    }
+}
---
+++ src/main/java/MergeTwoSortedLists.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MergeTwoSortedLists {
+
+    public static void main(String[] args) {
+      List<Integer> l1=new ArrayList<>();
+      l1.add(1);l1.add(2);l1.add(4);
+      List l2=l1=new ArrayList<>();
+      l2.add(1);l2.add(2);l2.add(4);
+
+        System.out.println( mergeTwoLists(l1,l2));
+
+    }
+
+
+    public static List<Integer> mergeTwoLists(List<Integer> list1, List<Integer> list2) {
+        List<Integer> l=new ArrayList<>();
+        for(int i=0; i<list1.size();i++){
+        for(int j=0;j<list2.size();j++){
+                if(list1.get(i)<list2.get(j)){
+                    l.add(list1.get(i));
+                }
+                else if(list1.get(i)>list2.get(j)){
+                    l.add(list1.get(j));
+                }
+                else{
+                    l.add(list1.get(j));
+                    l.add(list1.get(i));
+                }
+            }
+
+        } return l;
+
+    }
+}
